baca2b9bae07dca473c1ddb052a16fce088bd0ba
Oliver Plummer <100175003@student.burnley.ac.uk>
2022-02-02 15:54:36 +0000

    nhgmh

---
+++ ConsoleAppProject/JAVA/calculator/src/calculator/Calculator.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author 100175003
+ */
+public class Calculator {
+
+    String name;
+    
+   
+    
+    public int add(int x, int y){
+        return x+y;
+    }   
+    public int subtract(int x, int y){
+        return x-y;
+    }   
+    public int multply(int x, int y){
+        return x*y;
+    }   
+    public int divide(int x, int y){
+        return x/y;
+    }   
+    public int modulus(int x, int y){
+        return x+y;
+    }   
+
+}
+    
+}
