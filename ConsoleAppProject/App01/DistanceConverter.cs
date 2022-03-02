ed0bc463a05dc335f9da32ee3b21e37741aab115
Oliver Plummer <100175003@student.burnley.ac.uk>
2022-03-02 14:30:03 +0000

    completed Miles to Feet

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -9,15 +9,27 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        public const int FEET_IN_MILES = 5280;
+
         private double miles;
 
         private double feet;
         public void run()
         {
+            OutputHeading();
             inputMiles();
             CalculateFeet();
             OutputFeet();
         }
+
+        private void OutputHeading()
+        {
+            Console.WriteLine("\n-----------------------------");
+            Console.WriteLine("   convert Miles to Feet");
+            Console.WriteLine(     "by Oliver Plummer");
+            Console.WriteLine("------------------------------\n");
+        }
+
         private void inputMiles()
         {
             Console.Write("please enter the number of miles > ");
@@ -27,12 +39,12 @@ namespace ConsoleAppProject.App01
 
         private void CalculateFeet()
         {
-            
+            feet = miles * 5280; 
         
         }
         private void OutputFeet()
         {
-
+            Console.WriteLine(miles + "miles is" + feet + "feet!");
         } 
     }
 }
